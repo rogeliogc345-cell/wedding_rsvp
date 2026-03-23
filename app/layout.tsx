840b7b7baaf2fb62729de2a3ccb9e70f81348adc
Rogelio <gomez.rogelio@nokia.com>
2026-03-22 18:58:33 -0600

    testing to delete black mode in devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="light" style={{colorScheme:'light'}}>
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
